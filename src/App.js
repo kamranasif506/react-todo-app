4067854135c03112dc31e946337ddbdceaa3887e
Kamran Asif <98281453+kamranasif506@users.noreply.github.com>
2023-07-10 23:48:13 +0500

    remove navbar as it was not needed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import ToDoApp from "./components/ToDoApp";
-import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
       <ToDoApp />
     </div>
   );
